fa645000928512b55f0da6b9e2cb0b1631cd2910
Gabriel Dib <gabrieldib@gmail.com>
2025-02-20 22:56:33 -0800

    clean up (no node_modules, no /dist)

--- server/dist/handler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const handler = async (req, res) => {
-    res.end("Hello World");
-};
-exports.handler = handler;
--- server/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = require("http");
-const handler_1 = require("./handler");
-const port = 3000;
-const server = (0, http_1.createServer)(handler_1.handler);
-server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
