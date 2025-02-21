bb7979bb848382e80359672c517c10c4eb7a5859
Gabriel Dib <gabrieldib@gmail.com>
2025-02-20 22:52:45 -0800

    minimum nodejs server running

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import { createServer } from 'http';
+import { handler } from './handler';
+
+
+const port = 3000;
+const server = createServer(handler);
+
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
